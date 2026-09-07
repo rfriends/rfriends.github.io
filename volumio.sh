b536a0e8f5698a34949ea23812af10a565e8fe3e
mapi <rfriends2017@ymail.ne.jp>
2026-09-08 06:15:12 +0900

    Refactor volumio.sh for improved installation process
    Updated script to use bash and added checks for Volumio OS.

--- volumio.sh
+++ volumio.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
-cd ~/
-sudo apt-get install git -y
+#!/bin/bash
+#
+# rfriends3 for Volumio 2026/08/19
+# easy install script
+#
+echo easy install Ver 1.0
+echo
 
+if cat /etc/os-release | grep -q "volumio"; then
+    echo "このOSは Volumioです"
+else
+    echo "Volumioではありません"
+    exit 1
+fi
+
+cd ~/  
 rm -rf rfriends_volumio
+sudo apt-get install git  
 git clone https://github.com/rfriends/rfriends_volumio.git  
 cd rfriends_volumio  
-sh rfriends_volumio.sh
+sh rfriends_volumio.sh 
