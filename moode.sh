da973702a0d486f5299d6859bd3b0c0f0701d5fd
mapi <rfriends2017@ymail.ne.jp>
2026-09-08 06:15:51 +0900

    Update moode.sh

--- moode.sh
+++ moode.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
-cd ~/
-sudo apt-get install git -y
+#!/bin/bash
+#
+# rfriends3 for Moode 2026/08/19
+# easy install script
+#
+echo easy install Ver 1.0
+echo
 
-rm -rf rfriends_moode
+if command -v moodeutl >/dev/null 2>&1; then
+    echo "このOSは Moode Audioです。"
+else
+    echo "このOSは Moode Audioではありません。"
+    exit 1
+fi
+
+cd ~/  
+sudo apt install git  
+rm -rf rfriends_moode  
 git clone https://github.com/rfriends/rfriends_moode.git  
 cd rfriends_moode  
-sh rfriends_moode.sh
+sh rfriends_moode.sh 
